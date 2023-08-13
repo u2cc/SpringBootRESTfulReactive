0390e64ea36edd28e0093755b08f1de056a540dc
u2cc <cheng.james.chen@gmail.com>
2023-08-13 14:32:37 -0400

    Introduce Query annotation in custom lookup method

--- src/main/java/com/reactive/repository/DiecastCarRepository.java
+++ src/main/java/com/reactive/repository/DiecastCarRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
-import java.util.Collection;
 import java.util.List;
 
 /**
